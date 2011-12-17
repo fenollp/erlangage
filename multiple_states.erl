54e91e5f378dd39edbe7831ab03d76d2fa0d1274
Pierre Fenoll <pierrefenoll@gmail.com>
2011-12-17 02:33:49 +0100

    ➠ TextMate's ⌘S

---
+++ multiple_states.erl
@@ -0,0 +1,16 @@
+%%%----------------------------------------------------------------------
+%%% File    : multiple_states.erl
+%%% Author  : Pierre Fenoll ‹pierrefenoll@gmail.com›
+%%% Purpose : See multiple states of a looping function
+%%% Date    : 17 Dec 2011
+%%%----------------------------------------------------------------------
+
+-module(multiple_states).
+-author('pierrefenoll@gmail.com').
+
+-export([main/0]).
+
+main() -> main(0).
+main(11) -> ok;
+main(N) ->
+  receive .
