a2ac9b200a4d179b696066992152b956110bc687
Pierre Fenoll <pierrefenoll@gmail.com>
2014-03-17 17:08:56 +0100

    Was hanging around…

---
+++ badarith.erl
@@ -0,0 +1,18 @@
+%% Copyright © 2013 Pierre Fenoll ‹pierrefenoll@gmail.com›
+%% See LICENSE for licensing information.
+%% -*- coding: utf-8 -*-
+-module(badarith).
+
+%% badarith: 
+
+-export([ main/0 ]).
+
+
+%% API
+
+main () ->
+    1 + a.
+
+%% Internals
+
+%% End of Module.
