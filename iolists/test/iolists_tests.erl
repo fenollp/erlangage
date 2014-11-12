85c82b88be0aeab47be36113aa8abc9186a966d8
Pierre Fenoll <pierrefenoll@gmail.com>
2014-11-12 20:54:45 +0100

    map/2. filtermap/2. working on foldr.

--- iolists/test/iolists_tests.erl
+++ iolists/test/iolists_tests.erl
@@ -17,7 +17,7 @@ to_list_test_ () ->
                    , iolists:to_list(<<1,2,3>>))
     , ?_assertEqual( [1,2,3,4,5,6,7,8]
                    , iolists:to_list([<<1,2,3>>, 4, [5,6|7], [[[8]]]]))
-    , ?_assertException( error, badarg
+    , ?_assertException( error, function_clause
                        , iolists:to_list([<<1,2,3>>, 4, [5|6], {a}])) ].
 
 to_binary_test_ () ->
@@ -25,6 +25,37 @@ to_binary_test_ () ->
                    , iolists:to_binary([1,2,3, <<>>, 255]))
     , ?_assertEqual( <<1,2,3,1,1>>
                    , iolists:to_binary([1,2,3, <<>>, 257])) ].
+
+size_test_ () ->
+    [ ?_assertEqual( 6
+                   , iolists:size([1,2|<<1,2,257:16>>]))
+    , ?_assertEqual( 1
+                   , iolists:size(1)) ].
+
+map_test () ->
+    Id = fun (X) -> X end,
+    ?_assertEqual( lists:seq(1,10)
+                 , iolists:mapfilter(Id, [1,2,3, <<4,5,6>>,7,[[[8,9],10]]])).
+
+foldr_test_ () ->
+    LCons = fun (E,Acc) ->  [E|Acc]         end,
+    BCons = fun (E,Acc) -> <<E,Acc/binary>> end,
+    [ ?_assertEqual( <<1,2,3>>
+                   , iolists:foldr(BCons, <<>>, [1,2,3]))
+    , ?_assertEqual( [1,2,3]
+                   , iolists:foldr(LCons, [], [1,[2,<<3>>]])) ].
+
+filtermap_test_ () ->
+    LETwo = fun (X) -> X =< 2 end,
+    Intricate = fun
+                    (X) when X < 5 -> false;
+                    (X) when X > 5 -> true;
+                    (X) -> {true,cinq}
+                end,
+    [ ?_assertEqual( [1,2]
+                   , iolists:filtermap(LETwo, [1,2,3,4]))
+    , ?_assertEqual( [cinq,6,7,8,9,10]
+                   , iolists:filtermap(Intricate, lists:seq(1,10))) ].
 
 %% Internals
 
